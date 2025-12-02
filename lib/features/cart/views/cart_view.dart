339b7424a71dcac15afb2a70f4e11cc7afe0b256
01220330646zZ <ziad.abdo.2014@gmail.com>
2025-12-02 10:01:10 +0200

    Refactor CartView to be a StatefulWidget, implement item count management with increment and decrement functions, and enhance UI with improved app bar and bottom sheet styling

--- lib/features/cart/views/cart_view.dart
+++ lib/features/cart/views/cart_view.dart
@@ -2,33 +2,70 @@ import 'package:dota_burger/features/cart/views/widgets/cart_item.dart';
 import 'package:dota_burger/shared/custom_button.dart';
 import 'package:dota_burger/shared/custom_text.dart';
 import 'package:flutter/material.dart';
-import 'package:gap/gap.dart';
 
-class CartView extends StatelessWidget {
+class CartView extends StatefulWidget {
   const CartView({super.key});
+
+  @override
+  State<CartView> createState() => _CartViewState();
+}
+
+class _CartViewState extends State<CartView> {
+  int numberOfItems = 0;
+  void increment() {
+    setState(() {
+      numberOfItems++;
+    });
+  }
+
+  void decrement() {
+    setState(() {
+      if (numberOfItems > 1) {
+        numberOfItems--;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+
+        toolbarHeight: 0,
+        backgroundColor: Colors.white,
+        scrolledUnderElevation: 0,
+      ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 15.0),
         child: ListView.builder(
-          padding: EdgeInsets.only(top: 30, bottom: 120),
+          padding: EdgeInsets.only( bottom: 120),
           itemCount: 10,
           itemBuilder: (context, index) => CartItem(
             image: 'assets/test/t1.png',
             text: 'Classic Burger',
             desc: 'With cheese, lettuce, tomato',
-            numberOfItems: 1,
-            onAdd: () {},
-            onMinus: () {},
+            numberOfItems: numberOfItems,
+            onAdd: increment,
+            onMinus: decrement,
             onRemove: () {},
           ),
         ),
       ),
-
+      
       bottomSheet: Container(
-        color: Colors.white,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30),
+            topRight: Radius.circular(30),
+          ),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.grey,
+              blurRadius: 10,
+            ),
+          ]
+        ),
         padding: EdgeInsets.all(15),
         height: 100,
         child: Row(
